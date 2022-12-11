912bb8f6b7af9bc74c8e374adeaa2e0205a70d46
Keemchard <keemchardtamio@gmail.com>
2022-12-11 19:45:05 +0800

    🎨 format sec 3

--- src/pages/main-page/sections/Section3.tsx
+++ src/pages/main-page/sections/Section3.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Section3 = () => {
-  return <div>Section3</div>;
+  return (
+    <div className="bg-[brown] my-[20px] rounded flex flex-col justify-center items-center">
+      <div className="text-[1.5rem] border-[1px] py-[10px] px-[1rem] my-[20px]">
+        Date and Time
+      </div>
+      <div className="bg-[salmon]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        assumenda similique, pariatur, placeat iure unde quasi provident laborum
+        possimus magnam sit consectetur architecto, tempore quis eius minima
+        doloremque! Quo, quidem?
+      </div>
+    </div>
+  );
 };
 
 export default Section3;
