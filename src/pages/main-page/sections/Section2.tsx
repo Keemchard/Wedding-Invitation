6b811868f8a798027222f48152842f59c4b01c07
Keemchard <keemchardtamio@gmail.com>
2022-12-11 16:53:06 +0800

    🎨 modify section 2

--- src/pages/main-page/sections/Section2.tsx
+++ src/pages/main-page/sections/Section2.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import CoupleImage from "../../../assets/mock/sec-2-mock.jpg";
 
 const Section2 = () => {
   return (
-    <div className="flex">
-      <div>img con</div>
+    <div className="flex bg-[aqua]">
       <div>
-        <p className="font-bold">Description</p>
+        <img src={CoupleImage} alt="couple img" className="w-[40rem]" />
+      </div>
+      <div className="bg-[gray] m-[10px]">
+        <p className="font-bold text-[1.5rem]">Description</p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
         repudiandae sunt natus obcaecati suscipit tenetur cum libero nemo unde
         expedita.
