3dc0ba5109f34c19b41cb7bb6817a7aef7b3e756
Keemchard <keemchardtamio@gmail.com>
2022-12-13 18:04:30 +0800

    💄 update style

--- src/pages/main-page/sections/components/Event.tsx
+++ src/pages/main-page/sections/components/Event.tsx
@@ -12,13 +12,15 @@ const Event: FC<EventProp> = ({ timeline }) => {
         className="bg-[gray] m-[10px] mb-[20px] flex"
         style={{ justifyContent: box_position }}
       >
-        <div className="bg-[red] w-[50%] px-[2.5rem] relative">
-          <p>{time}</p>
-          <p className="text-[20px] font-bold">{title}</p>
-          <p>{description}</p>
-          <p>{box_position}</p>
+        <div className="timeline-con w-[50%] px-[2.5rem] relative">
+          <div className="hover:scale-[1.1] transition-[0.25s]">
+            <p>{time}</p>
+            <p className="text-[20px] font-bold">{title}</p>
+            <p>{description}</p>
+            <p>{box_position}</p>
+          </div>
           <div
-            className="bg-[blue] absolute h-[1.5rem] w-[1.5rem] rounded-[50%] flex justify-center top-0 z-20"
+            className="timeline-circle bg-[blue] absolute h-[1.5rem] w-[1.5rem] rounded-[50%] flex justify-center top-0 z-20"
             style={
               box_position === "left"
                 ? { right: "-0.75rem" }
