39359069457d8506ce4a4bda36db2ee861db69b1
Keemchard <keemchardtamio@gmail.com>
2022-12-18 16:33:53 +0800

    🤡 mock test scroll animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/main-page/MainPage";
+import { motion, useScroll } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
   return (
     <>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
