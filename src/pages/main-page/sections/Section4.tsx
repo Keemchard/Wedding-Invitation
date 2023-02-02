867cb771e68450f0cc24b15c046a154ffef18f45
Keemchard <keemchardtamio@gmail.com>
2023-02-02 21:52:33 +0800

    ✨ fetch livestream link

--- src/pages/main-page/sections/Section4.tsx
+++ src/pages/main-page/sections/Section4.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Button from "../../../components/Button";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase/firebase-config";
+import Loading from "../../../components/Loading";
 
 const Section4 = () => {
+  const [loading, setLoading] = useState(false);
+  const [livestreamLink, setLivestreamLink] = useState("");
+
   const goToLivestream = () => {
-    alert("this is the livestream");
+    getLivestreamLink();
   };
+
+  const getLivestreamLink = async () => {
+    try {
+      setLoading(true);
+      const querySnapshot = await getDocs(collection(db, "livestreamLink"));
+      const link = querySnapshot.docs.map((item) => {
+        return item.data();
+      });
+      setLivestreamLink(link[0].link);
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getLivestreamLink();
+  }, []);
+
   return (
     <div className="sec-4-con p-[1rem] flex justify-center my-[5rem]">
       <div className="bg-wd_coffee p-[1rem] flex gap-3">
@@ -13,13 +38,21 @@ const Section4 = () => {
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
         </div>
         <div className="min-w-[170px] text-right">
-          <Button
-            label="Join Now"
-            // icon="uil uil-tv-retro"
-            btnFunction={goToLivestream}
-            // bgColor="#CCB494"
-            // txtColor="black"
-          />
+          {loading ? (
+            <Loading solo={false} />
+          ) : (
+            <a href={livestreamLink} target="_blank" rel="noreferrer">
+              <div>
+                <Button
+                  label="Join Now"
+                  // icon="uil uil-tv-retro"
+                  btnFunction={goToLivestream}
+                  // bgColor="#CCB494"
+                  // txtColor="black"
+                />
+              </div>
+            </a>
+          )}
         </div>
       </div>
     </div>
