9390608972dc9e982b07a715ff708e490a0d7fc7
Keemchard <keemchardtamio@gmail.com>
2023-02-21 23:28:43 +0800

    refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-wd_coffee text-white py-7 mt-32 flex gap-2 justify-center items-center">
-      <div>
-        <span className="text-wd_mikado_yellow">She</span>ila And Ji
-        <span className="text-wd_mikado_yellow">mel</span>
-      </div>
-      <div>
-        Wedding Invitation{" "}
-        <span className="text-wd_mikado_yellow">2023 ✨</span>
-      </div>
+    <div className="bg-wd_coffee text-white py-7 mt-32 flex justify-center items-center">
+      <p className="footer-txt">
+        God has made everything beautiful in its time. ~ Ecclesiastes 3:11
+      </p>
     </div>
   );
 };
